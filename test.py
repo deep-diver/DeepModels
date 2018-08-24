7ddfd0765f12c53bc492d12836a85fa781ba7b21
Chansung <deep.diver.csp@gmail.com>
2018-08-24 10:43:52 +0900

    adding the test code statements for VGG and CIFAR100 dataset

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import sys
 import tensorflow as tf
 
 from dataset.cifar10_dataset import Cifar10
+from dataset.cifar100_dataset import Cifar100
 from models.alexnet import AlexNet
+from models.vgg import VGG
 from trainers.clftrainer import ClfTrainer
 
 def main():
@@ -15,10 +17,19 @@ def main():
 
     print('hello world')
     cifar10_dataset = Cifar10()
-    alexNet = AlexNet()
+    # cifar100_dataset = Cifar100()
 
-    input, output = alexNet.set_dataset(cifar10_dataset)
-    model_final_layer = alexNet.create_model(input)
+    # AlexNet Test Statements
+    # alexNet = AlexNet()
+    #
+    # input, output = alexNet.set_dataset(cifar100_dataset)
+    # model_final_layer = alexNet.create_model(input)
+    # logits = tf.identity(model_final_layer, name='logits')
+
+    vgg = VGG()
+    # input, output = vgg.set_dataset(cifar100_dataset)
+    input, output = vgg.set_dataset(cifar10_dataset)
+    model_final_layer = vgg.create_model(input, options={'model_type': 'A'})
     logits = tf.identity(model_final_layer, name='logits')
 
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=output), name='cost')
@@ -27,7 +38,9 @@ def main():
     correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(output, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='accuracy')
 
-    trainer = ClfTrainer(alexNet, cifar10_dataset)
+    # trainer = ClfTrainer(alexNet, cifar100_dataset)
+    # trainer = ClfTrainer(vgg, cifar100_dataset)
+    trainer = ClfTrainer(vgg, cifar10_dataset)
     trainer.train(input, output, cost, optimizer, accuracy, epochs, batch_size, save_model_path)
 
 if __name__ == "__main__":
