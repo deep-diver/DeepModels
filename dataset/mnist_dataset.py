d57bc792a8c8356311304af1a1cd13820d31e4d3
Chansung <deep.diver.csp@gmail.com>
2018-08-28 01:29:06 +0900

    downloading codes

--- dataset/mnist_dataset.py
+++ dataset/mnist_dataset.py
@@ -20,6 +20,47 @@ class Mnist(Dataset):
         self.height = 28
 
     def download(self):
+        # training dataset
+        if not isfile('train-images-idx3-ubyte.gz'):
+            with DownloadProgress(unit='B', unit_scale=True, miniters=1, desc='MNIST training dataset (images)') as pbar:
+                urlretrieve(
+                    'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
+                    'train-images-idx3-ubyte.gz',
+                    pbar.hook)
+        else:
+            print('train-images-idx3-ubyte.gz already exists')
+
+        if not isfile('train-labels-idx1-ubyte.gz'):
+            with DownloadProgress(unit='B', unit_scale=True, miniters=1, desc='MNIST training dataset (labels)') as pbar:
+                urlretrieve(
+                    'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
+                    'train-labels-idx1-ubyte.gz',
+                    pbar.hook)
+        else:
+            print('train-labels-idx1-ubyte.gz already exists')
+
+        # testing dataset
+        if not isfile('t10k-images-idx3-ubyte.gz'):
+            with DownloadProgress(unit='B', unit_scale=True, miniters=1, desc='MNIST testing dataset (images)') as pbar:
+                urlretrieve(
+                    'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz',
+                    't10k-images-idx3-ubyte.gz',
+                    pbar.hook)
+        else:
+            print('t10k-images-idx3-ubyte.gz already exists')
+
+        if not isfile('t10k-labels-idx1-ubyte.gz'):
+            with DownloadProgress(unit='B', unit_scale=True, miniters=1, desc='MNIST testing dataset (labels)') as pbar:
+                urlretrieve(
+                    'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz',
+                    't10k-labels-idx1-ubyte.gz',
+                    pbar.hook)
+        else:
+            print('t10k-labels-idx1-ubyte.gz already exists')
+
+        # unzip
+        
+
         raise NotImplementedError
 
     def preprocess_and_save_data(self):
