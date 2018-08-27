0baab011a20f2ed7a0a94a976472c10cb3cddc79
CHANSUNG PARK <deep.diver.csp@gmail.com>
2018-08-27 13:28:35 +0900

    up to inception (3b)

--- models/googlenet.py
+++ models/googlenet.py
@@ -29,31 +29,57 @@ class GoogLeNet(ImgClfModel):
         self.pool_2 = max_pool2d(self.lrn_2, kernel_size=[3,3], stride=2)
 
         # Inception (3a)
-        conv2d_1 = conv2d(out, num_outputs=64,
+        conv2d_1 = conv2d(self.pool_2, num_outputs=64,
                     kernel_size=[1,1], stride=1, padding="SAME",
                     activation_fn=tf.nn.relu)
 
-        conv2d_2 = conv2d(out, num_outputs=96,
+        conv2d_2 = conv2d(self.pool_2, num_outputs=96,
                     kernel_size=[1,1], stride=1, padding="SAME",
                     activation_fn=tf.nn.relu)
         conv2d_2 = conv2d(conv2d_2, num_outputs=128,
                     kernel_size=[3,3], stride=1, padding="SAME",
                     activation_fn=tf.nn.relu)
 
-        conv2d_3 = conv2d(out, num_outputs=16,
+        conv2d_3 = conv2d(self.pool_2, num_outputs=16,
                     kernel_size=[1,1], stride=1, padding="SAME",
                     activation_fn=tf.nn.relu)
         conv2d_3 = conv2d(conv2d_3, num_outputs=32,
                     kernel_size=[5,5], stride=1, padding="SAME",
                     activation_fn=tf.nn.relu)
 
-        conv2d_4 = max_pool2d(out, kernel_size=[3,3], stride=1)
+        conv2d_4 = max_pool2d(self.pool_2, kernel_size=[3,3], stride=1)
         conv2d_4 = conv2d(conv2d_4, num_outputs=32,
                     kernel_size=[1,1], stride=1, padding="SAME",
                     activation_fn=tf.nn.relu)
-        inception_1 = tf.concat(3, [conv2d_1, conv2d_2, conv2d_3, conv2d_4])
+        self.inception_1 = tf.concat(3, [conv2d_1, conv2d_2, conv2d_3, conv2d_4])
 
         # inception (3b)
+        conv2d_1 = conv2d(self.inception_1, num_outputs=128,
+                    kernel_size=[1,1], stride=1, padding="SAME",
+                    activation_fn=tf.nn.relu)
+
+        conv2d_2 = conv2d(self.inception_1, num_outputs=128,
+                    kernel_size=[1,1], stride=1, padding="SAME",
+                    activation_fn=tf.nn.relu)
+        conv2d_2 = conv2d(conv2d_2, num_outputs=192,
+                    kernel_size=[3,3], stride=1, padding="SAME",
+                    activation_fn=tf.nn.relu)
+
+        conv2d_3 = conv2d(self.inception_1, num_outputs=32,
+                    kernel_size=[1,1], stride=1, padding="SAME",
+                    activation_fn=tf.nn.relu)
+        conv2d_3 = conv2d(conv2d_3, num_outputs=96,
+                    kernel_size=[5,5], stride=1, padding="SAME",
+                    activation_fn=tf.nn.relu)
+
+        conv2d_4 = max_pool2d(self.inception_1, kernel_size=[3,3], stride=1)
+        conv2d_4 = conv2d(conv2d_4, num_outputs=64,
+                    kernel_size=[1,1], stride=1, padding="SAME",
+                    activation_fn=tf.nn.relu)
+        self.inception_2 = tf.concat(3, [conv2d_1, conv2d_2, conv2d_3, conv2d_4])
+        self.inception_2_pool_1 = max_pool2d(self.inception_2, kernel_size=[3,3], stride=2)
+
+        # inception (4a)
 
     def load_pretrained_model(self, save_model_from, options):
         raise NotImplementedError
