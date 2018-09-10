85af2125bc1fbb6fee7b70785cb5d551fb618216
chansung park <deep.diver.csp@gmail.com>
2018-09-11 01:08:29 +0900

    inceptionv2 stem part

--- models/inception_v2.py
+++ models/inception_v2.py
@@ -10,7 +10,28 @@ from tensorflow.contrib.layers import fully_connected
 
 class InceptionV2(ImgClfModel):
     def __init__(self):
-        ImgClfModel.__init__(self, scale_to_imagenet=True)
+        ImgClfModel.__init__(self, scale_to_imagenet=False)
 
     def create_model(self, input, options=None):
+        # STEM Network
+        with tf.variable_scope('stem'):
+            self.conv2d_1 = conv2d(input, num_outputs=32,
+                        kernel_size=[3,3], stride=2, padding="VALID",
+                        activation_fn=tf.nn.relu)
+            self.conv2d_2 = conv2d(self.conv2d_1, num_outputs=32,
+                                    kernel_size=[3,3], stride=1, padding="VALID",
+                                    activation_fn=tf.nn.relu)
+            self.conv2d_3 = conv2d(self.conv2d_2, num_outputs=64,
+                                    kernel_size=[3,3], stride=1, padding="SAME",
+                                    activation_fn=tf.nn.relu)
+            self.pool_1 = max_pool2d(self.conv2d_3, kernel_size=[3,3], stride=2, padding='VALID')
+            self.conv2d_4 = conv2d(self.pool_1, num_outputs=80,
+                                    kernel_size=[3,3], stride=1, padding="VALID",
+                                    activation_fn=tf.nn.relu)
+            self.conv2d_5 = conv2d(self.conv2d_4, num_outputs=192,
+                                    kernel_size=[3,3], stride=2, padding="VALID",
+                                    activation_fn=tf.nn.relu)
+            self.conv2d_6 = conv2d(self.conv2d_5, num_outputs=288,
+                                    kernel_size=[3,3], stride=1, padding="SAME",
+                                    activation_fn=tf.nn.relu)    
         raise NotImplementedError
