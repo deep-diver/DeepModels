f015d438b8d32b09f83d3a9274533406946dfe41
chansung park <deep.diver.csp@gmail.com>
2018-09-11 21:24:13 +0900

    naive implementation of InceptionV2. Not tested yet

--- models/inception_v2.py
+++ models/inception_v2.py
@@ -35,7 +35,8 @@ class InceptionV2(ImgClfModel):
             self.pool_2 = max_pool2d(self.conv2d_5, kernel_size=[3,3], stride=2, padding='VALID')
 
             prev = self.pool_2
-            # inception 3
+
+            # inception 3-1
             branch_a = conv2d(prev, num_outputs=64,
                               kernel_size=[1,1], stride=1, padding='SAME')
 
@@ -52,6 +53,8 @@ class InceptionV2(ImgClfModel):
                               kernel_size=[3,3], stride=1, padding='SAME')
 
             branch_d = avg_pool2d(self.pool_2, kernel_size=[3,3], stride=2, padding='SAME'))
+            branch_d = conv2d(branch_d, num_outputs=32,
+                              kernel_size=[1,1], stride=1, padding='SAME')
 
             layers_concat = list()
             layers_concat.append(branch_a)
@@ -60,8 +63,303 @@ class InceptionV2(ImgClfModel):
             layers_concat.append(branch_d)
             prev = tf.concat(layers_concat, 3)
 
-            # inception 5
+            # inception 3-2
+            branch_a = conv2d(prev, num_outputs=64,
+                              kernel_size=[1,1], stride=1, padding='SAME')
 
-            # inception 2
+            branch_b = conv2d(prev, num_outputs=48,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=64,
+                              kernel_size=[3,3], stride=1, padding='SAME')
 
-        raise NotImplementedError
+            branch_c = conv2d(prev, num_outputs=64,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=96,
+                              kernel_size=[3,3], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=96,
+                              kernel_size=[3,3], stride=1, padding='SAME')
+
+            branch_d = avg_pool2d(prev, kernel_size=[3,3], stride=2, padding='SAME'))
+            branch_d = conv2d(branch_d, num_outputs=64,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            layers_concat.append(branch_d)
+            prev = tf.concat(layers_concat, 3)
+
+            # inception 3-3
+            branch_a = conv2d(prev, num_outputs=64,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+
+            branch_b = conv2d(prev, num_outputs=48,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=64,
+                              kernel_size=[3,3], stride=1, padding='SAME')
+
+            branch_c = conv2d(prev, num_outputs=64,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=96,
+                              kernel_size=[3,3], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=96,
+                              kernel_size=[3,3], stride=1, padding='SAME')
+
+            branch_d = avg_pool2d(prev, kernel_size=[3,3], stride=2, padding='SAME'))
+            branch_d = conv2d(branch_d, num_outputs=64,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+                              
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            layers_concat.append(branch_d)
+            prev = tf.concat(layers_concat, 3)
+
+            # inception 5-1 (grid size reduction)
+            branch_a = conv2d(prev, num_outputs=384, 
+                              kernel_size=[3,3], stride=2, padding='VALID')
+
+            branch_b = conv2d(prev, num_outputs=64,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=96,
+                              kernel_size=[3,3], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=96,
+                              kernel_size=[3,3], stride=2, padding='VALID')
+
+            branch_c = max_pool2d(prev, kernel_size=[3,3], stride=2, padding='VALID')
+
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            prev = tf.concat(layers_concat, 3)
+
+            # inception 5-2
+            branch_a = conv2d(prev, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            
+            branch_b = conv2d(prev, num_outputs=128,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=128,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=192,
+                              kernel_size=[7,1], stride=1, padding='SAME')
+
+            branch_c = conv2d(prev, num_outputs=128,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=128,
+                              kernel_size=[7,7], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=128,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=128,
+                              kernel_size=[7,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=192,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+
+            branch_d = avg_pool2d(prev, kernel_size=[3,3], stride=2, padding='SAME'))
+            branch_d = conv2d(branch_d, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            layers_concat.append(branch_d)
+            prev = tf.concat(layers_concat, 3)
+
+            # inception 5-3
+            branch_a = conv2d(prev, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            
+            branch_b = conv2d(prev, num_outputs=160,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=160,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=192,
+                              kernel_size=[7,1], stride=1, padding='SAME')
+
+            branch_c = conv2d(prev, num_outputs=160,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=160,
+                              kernel_size=[7,7], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=160,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=160,
+                              kernel_size=[7,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=192,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+
+            branch_d = avg_pool2d(prev, kernel_size=[3,3], stride=2, padding='SAME'))
+            branch_d = conv2d(branch_d, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            layers_concat.append(branch_d)
+            prev = tf.concat(layers_concat, 3)
+
+            # inception 5-4
+            branch_a = conv2d(prev, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            
+            branch_b = conv2d(prev, num_outputs=160,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=160,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=192,
+                              kernel_size=[7,1], stride=1, padding='SAME')
+
+            branch_c = conv2d(prev, num_outputs=160,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=160,
+                              kernel_size=[7,7], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=160,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=160,
+                              kernel_size=[7,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=192,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+
+            branch_d = avg_pool2d(prev, kernel_size=[3,3], stride=2, padding='SAME'))
+            branch_d = conv2d(branch_d, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            layers_concat.append(branch_d)
+            prev = tf.concat(layers_concat, 3)
+
+            # inception 5-5
+            branch_a = conv2d(prev, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            
+            branch_b = conv2d(prev, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=192,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=192,
+                              kernel_size=[7,1], stride=1, padding='SAME')
+
+            branch_c = conv2d(prev, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=192,
+                              kernel_size=[7,7], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=192,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=192,
+                              kernel_size=[7,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=192,
+                              kernel_size=[1,7], stride=1, padding='SAME')
+
+            branch_d = avg_pool2d(prev, kernel_size=[3,3], stride=2, padding='SAME'))
+            branch_d = conv2d(branch_d, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            layers_concat.append(branch_d)
+            prev = tf.concat(layers_concat, 3)
+            self.aux = pref
+
+            # inception 2-1 (grid size reduction)
+            branch_a = conv2d(prev, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_a = conv2d(branch_a, num_outputs=320,
+                              kernel_size=[3,3], stride=2, padding='VALID')
+
+            branch_b = conv2d(prev, num_outputs=192,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=192,
+                              kernel_size=[1,7], stride=1, padding='SAME')                              
+            branch_b = conv2d(branch_b, num_outputs=192,
+                              kernel_size=[7,1], stride=1, padding='SAME')  
+            branch_b = conv2d(branch_b, num_outputs=192,
+                              kernel_size=[3,3], stride=2, padding='VALID')
+
+            branch_c = max_pool2d(prev, kernel_size=[3,3], stride=2, padding='VALID')
+
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            prev = tf.concat(layers_concat, 3)
+
+            # inception 2-2
+            branch_a = conv2d(prev, num_outputs=320,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+
+            branch_b = conv2d(prev, num_outputs=384, 
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=384,
+                              kernel_size=[1,3], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=384,
+                              kernel_size=[3,1], stride=1, padding='SAME')
+
+            branch_c = conv2d(prev, num_outputs=448,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=384,
+                              kernel_size=[1,3], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=384,
+                              kernel_size=[3,1], stride=1, padding='SAME')
+            
+            branch_d = max_pool2d(prev, kernel_size=[3,3], stride=2, padding='SAME')
+            branch_d = convd2d(branch_d, num_outputs=192,
+                               kernel_size=[1,1], stride=1, padding='SAME')
+
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            layers_concat.append(branch_d)
+            prev = tf.concat(layers_concat, 3)
+
+            # inception 2-3
+            branch_a = conv2d(prev, num_outputs=320,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+
+            branch_b = conv2d(prev, num_outputs=384, 
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=384,
+                              kernel_size=[1,3], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=384,
+                              kernel_size=[3,1], stride=1, padding='SAME')
+
+            branch_c = conv2d(prev, num_outputs=448,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=384,
+                              kernel_size=[1,3], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=384,
+                              kernel_size=[3,1], stride=1, padding='SAME')
+            
+            branch_d = max_pool2d(prev, kernel_size=[3,3], stride=2, padding='SAME')
+            branch_d = convd2d(branch_d, num_outputs=192,
+                               kernel_size=[1,1], stride=1, padding='SAME')
+                               
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            layers_concat.append(branch_d)
+            prev = tf.concat(layers_concat, 3)
+
+            self.aux_pool = avg_pool2d(self.aux, kernel_size=[5,5], stride=3, padding='VALID')
+            self.aux_conv = conv2d(self.aux_pool, num_outputs=128,
+                                   kernel_size=[1,1], stride=1, padding='SAME')
+            self.aux_flat = flatten(self.aux_conv)
+            self.aux_fcl = fully_connected(self.aux_flat, num_outputs=768, activation_fn=tf.nn.relu)
+            self.aux_out = fully_connected(self.aux_fcl, num_outputs=self.num_classes, activation_fn=None)
+
+            self.final_pool =  avg_pool2d(self.pool_2, kernel_size=[8,8], stride=2, padding='VALID'))
+            self.final_dropout = tf.nn.dropout(self.final_pool, 0.8)
+            self.final_flat = flatten(self.final_dropout)
+            self.final_out = fully_connected(self.final_flat, num_outputs=self.num_classes, activation_fn=None)
+            
+        return [self.aux_out, self.out]
