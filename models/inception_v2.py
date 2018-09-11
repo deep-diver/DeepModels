f790a2436c1991fda9b70984f87a1efc4177f4e6
CHANSUNG PARK <deep.diver.csp@gmail.com>
2018-09-11 17:29:51 +0900

    stem network is revised a bit with max pooling at the end. a unit for the inception3 is prototyped.

--- models/inception_v2.py
+++ models/inception_v2.py
@@ -16,22 +16,52 @@ class InceptionV2(ImgClfModel):
         # STEM Network
         with tf.variable_scope('stem'):
             self.conv2d_1 = conv2d(input, num_outputs=32,
-                        kernel_size=[3,3], stride=2, padding="VALID",
+                        kernel_size=[3,3], stride=2, padding='VALID',
                         activation_fn=tf.nn.relu)
             self.conv2d_2 = conv2d(self.conv2d_1, num_outputs=32,
-                                    kernel_size=[3,3], stride=1, padding="VALID",
+                                    kernel_size=[3,3], stride=1, padding='VALID',
                                     activation_fn=tf.nn.relu)
             self.conv2d_3 = conv2d(self.conv2d_2, num_outputs=64,
-                                    kernel_size=[3,3], stride=1, padding="SAME",
+                                    kernel_size=[3,3], stride=1, padding='SAME',
                                     activation_fn=tf.nn.relu)
             self.pool_1 = max_pool2d(self.conv2d_3, kernel_size=[3,3], stride=2, padding='VALID')
+
             self.conv2d_4 = conv2d(self.pool_1, num_outputs=80,
-                                    kernel_size=[3,3], stride=1, padding="VALID",
+                                    kernel_size=[3,3], stride=1, padding='VALID',
                                     activation_fn=tf.nn.relu)
             self.conv2d_5 = conv2d(self.conv2d_4, num_outputs=192,
-                                    kernel_size=[3,3], stride=2, padding="VALID",
+                                    kernel_size=[3,3], stride=2, padding='VALID',
                                     activation_fn=tf.nn.relu)
-            self.conv2d_6 = conv2d(self.conv2d_5, num_outputs=288,
-                                    kernel_size=[3,3], stride=1, padding="SAME",
-                                    activation_fn=tf.nn.relu)    
+            self.pool_2 = max_pool2d(self.conv2d_5, kernel_size=[3,3], stride=2, padding='VALID')
+
+            prev = self.pool_2
+            # inception 3
+            branch_a = conv2d(prev, num_outputs=64,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+
+            branch_b = conv2d(prev, num_outputs=48,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_b = conv2d(branch_b, num_outputs=64,
+                              kernel_size=[3,3], stride=1, padding='SAME')
+
+            branch_c = conv2d(prev, num_outputs=64,
+                              kernel_size=[1,1], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=96,
+                              kernel_size=[3,3], stride=1, padding='SAME')
+            branch_c = conv2d(branch_c, num_outputs=96,
+                              kernel_size=[3,3], stride=1, padding='SAME')
+
+            branch_d = avg_pool2d(self.pool_2, kernel_size=[3,3], stride=2, padding='SAME'))
+
+            layers_concat = list()
+            layers_concat.append(branch_a)
+            layers_concat.append(branch_b)
+            layers_concat.append(branch_c)
+            layers_concat.append(branch_d)
+            prev = tf.concat(layers_concat, 3)
+
+            # inception 5
+
+            # inception 2
+
         raise NotImplementedError
