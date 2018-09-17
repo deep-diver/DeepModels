47b15886c3f4c33d22909a197f04ffc198b51404
CHANSUNG PARK <deep.diver.csp@gmail.com>
2018-09-17 11:48:09 +0900

    adding template files inception+resnet v1 and v2.

---
+++ models/inception+resnet_v2.py
@@ -0,0 +1,16 @@
+from models.imgclfmodel import ImgClfModel
+from dataset.dataset import Dataset
+
+import tensorflow as tf
+from tensorflow.contrib.layers import conv2d
+from tensorflow.contrib.layers import max_pool2d
+from tensorflow.contrib.layers import avg_pool2d
+from tensorflow.contrib.layers import flatten
+from tensorflow.contrib.layers import fully_connected
+
+class Inception+ResnetV2(ImgClfModel):
+    def __init__(self):
+        ImgClfModel.__init__(self, scale_to_imagenet=True)
+
+    def create_model(self, input):
+        raise NotImplementedError
